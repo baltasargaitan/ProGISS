3ceeb4a515b488469254c8c87991f6349f4e52a1
Baltasar Gaitan <baltasargaitan12@gmail.com>
2025-01-29 06:33:30 -0300

    Preparando despliegue en Render

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn pro_giss.wsgi:application
--- pro_giss/settings.py
+++ pro_giss/settings.py
@@ -7,7 +7,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Security settings
 SECRET_KEY = os.getenv('SECRET_KEY', 'django-insecure-!#pt*pqi8z64!^2gyrl=+1gxcn7id&2$n01(waw&8f0u!j7i($')
 DEBUG = os.getenv('DEBUG', 'True') == 'True'
-ALLOWED_HOSTS = ['127.0.0.1', 'localhost']
+ALLOWED_HOSTS = ['127.0.0.1', 'localhost', os.getenv('RENDER_EXTERNAL_HOSTNAME', '')]
+
 
 
 # Application definition
@@ -23,6 +24,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',  # <- Agregar aquí
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -30,6 +32,7 @@ MIDDLEWARE = [
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
+
 
 ROOT_URLCONF = 'pro_giss.urls'
 
@@ -52,24 +55,12 @@ TEMPLATES = [
 WSGI_APPLICATION = 'pro_giss.wsgi.application'
 
 # Database configuration
-if os.getenv('DJANGO_ENV') == 'production':  # If in production, use PostgreSQL
-    DATABASES = {
-        'default': {
-            'ENGINE': 'django.db.backends.postgresql',
-            'NAME': os.getenv('DB_NAME'),  # Name of the PostgreSQL database
-            'USER': os.getenv('DB_USER'),  # PostgreSQL username
-            'PASSWORD': os.getenv('DB_PASSWORD'),  # PostgreSQL password
-            'HOST': os.getenv('DB_HOST'),  # PostgreSQL host (provided by Render, Heroku, etc.)
-            'PORT': os.getenv('DB_PORT', '5432'),  # Default PostgreSQL port
-        }
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
-else:  # In development, also connect to the same remote PostgreSQL database
-    DATABASES = {
-      'default': {
-        'ENGINE': 'django.db.backends.sqlite3',  # Usamos SQLite por defecto
-        'NAME': BASE_DIR / 'db.sqlite3',  # Archivo de base de datos en la raíz del proyecto
-    }
-    }
+}
 
 # Password validation
 AUTH_PASSWORD_VALIDATORS = [
@@ -98,6 +89,10 @@ STATIC_URL = 'static/'
 STATICFILES_DIRS = [
     BASE_DIR / "static",  # Path to the static folder in the project root
 ]
+
+STATIC_ROOT = BASE_DIR / "staticfiles"
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
 
 # Default primary key field type
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
